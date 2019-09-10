8b70f89498c6f46d28bc5f1f57c2c1eb29419ef4
Joshua-Shockley <thelumbashockley@gmail.com>
2019-09-10 13:47:00 -0500

    just getting started

---
+++ todo/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      see this in App.js
+      </header>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
